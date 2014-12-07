c9bf638efd98b3e920ad97adabd9a423176395fa
JonKofee <Sereban@xakep.ru>
2014-12-07 12:44:52 +0300

    Initial Commit

---
+++ Substitution cipher/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Substitution cipher
+//
+//  Created by Sergey Prischenko on 12/7/14.
+//  Copyright (c) 2014 Sergey Prischenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
